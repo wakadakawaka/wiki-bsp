df47dcbb8246d6d4ad315ac36c9072ca80b2914e
wakadakawaka <s-naa.91@yamdex.ru>
2025-04-25 21:34:34 +0500

    elfk

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,22 +12,11 @@ export default defineConfig({
         		// Relative path to your custom CSS file
       	  		'./src/styles/custom.css',
      		 ],
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://git.sinenikolsky.ru/artem/starlite-bsp-kb.git' }],
 			sidebar: [
 				{
 					collapsed: true,
 					label: 'Библиотека стандартных подсистем',
 					autogenerate: { directory: 'bsp' },
-				},
-				{
-					collapsed: true,
-					label: 'Зарплата и управление персоналом',
-					autogenerate: { directory: 'zup' },
-				},
-				{
-					collapsed: true,
-					label: 'ERP. Управление холдингом',
-					autogenerate: { directory: 'erp_uh' },
 				},
 			],
 		}),
